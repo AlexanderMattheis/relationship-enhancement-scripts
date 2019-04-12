adb6b5905b1e7e3fd6e2b172bd76bcab918ef892
AlexanderMattheis <almat@gmx.de>
2019-04-13 00:47:42 +0200

    Changed Paths + added script that converts DOC-files into PDF-files.

---
+++ src/RecursiveDocToPdfConverter.ps1
@@ -0,0 +1,24 @@
+# SCRIPT 2019-04-12, Powershell 5.1.17134.228, Alexander Mattheis
+
+# constants
+$PDF_FORMAT = 17;
+
+
+function Start-Script() {
+    $wordApplication = New-Object -ComObject Word.Application;
+
+    Get-ChildItem -Path $PSScriptRoot -Include *.doc, *.docx, *.rtf -Recurse |
+	    ForEach-Object {
+		    $docFile = $wordApplication.Documents.Open($_.Fullname);
+		    $pdfFileName = $_.FullName -replace $_.Extension, '.pdf';
+		    $docFile.ExportAsFixedFormat($pdfFileName, $PDF_FORMAT);
+		    $docFile.Close();
+
+            Write-Host $pdfFileName;
+	    }
+
+    $wordApplication.Quit();
+    Write-Host "DONE";
+}
+
+Start-Script;
